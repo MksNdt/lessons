656bbda1c3ce3c169765700a41e85baf5696958c
Maksim Nadtochaev <maksim.nadtochaev@gmail.com>
2024-05-29 20:04:25 +0300

    Update task9.java

--- src/task9.java
+++ src/task9.java
@@ -1,5 +1,5 @@
 public class task9 {
-    public static boolean ВисокосныйГод(int year) {
+    public static boolean leapYear(int year) {
         if (year % 4 != 0) {
             return false;
         } else if (year % 100 != 0) {
@@ -12,9 +12,9 @@ public class task9 {
     }
 
     public static void main(String[] args) {
-        System.out.println(ВисокосныйГод(2076));  // true
-        System.out.println(ВисокосныйГод(2100));  // false
-        System.out.println(ВисокосныйГод(2350));  // false
-        System.out.println(ВисокосныйГод(2000));  // true
+        System.out.println(leapYear(2076));  // true
+        System.out.println(leapYear(2100));  // false
+        System.out.println(leapYear(2350));  // false
+        System.out.println(leapYear(2000));  // true
     }
 }
