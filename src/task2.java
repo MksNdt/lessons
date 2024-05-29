c2d24a06e94ea2155a4eb83b8f36cbfa6499a597
Maksim Nadtochaev <maksim.nadtochaev@gmail.com>
2024-05-29 21:27:53 +0300

    Update task2.java

--- src/task2.java
+++ src/task2.java
@@ -1,11 +1,14 @@
-public class task2 {
+class task2 {
     public static void main(String[] args) {
+        checkSumSign();
+    }
+    public static void checkSumSign() {
         int a = 3;
         int b = -5;
-        if (a+b>0) {
-            System.out.println("Сумма положительная");
+        if (a + b >= 0) {
+            System.out.println("Amount is positive");
         }
         else
-            System.out.println("Сумма положительная");
+            System.out.println("Amount is negative");
     }
 }
