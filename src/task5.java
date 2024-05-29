a2fe647d95ddc3a928fd1dbcbfb082962c9b8f63
Maksim Nadtochaev <maksim.nadtochaev@gmail.com>
2024-05-29 20:51:35 +0300

    Update task5.java

--- src/task5.java
+++ src/task5.java
@@ -1,6 +1,6 @@
 class task5 {
     public static void main(String[] args) {
-        System.out.println(isSumInRange(13, 13));
+        System.out.println(isSumInRange(13, 15));
     }
     public static boolean isSumInRange(int a, int b) {
         int sum = a + b;
