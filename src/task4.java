db33eaf75ce6c4a3c7dcc962cb56b1ba4273772f
Maksim Nadtochaev <maksim.nadtochaev@gmail.com>
2024-05-29 21:05:39 +0300

    Update task4.java

--- src/task4.java
+++ src/task4.java
@@ -1,11 +1,13 @@
 public class task4 {
     public static void main(String[] args) {
+        compareNumbers();
+    }
+    public static void compareNumbers() {
         int a = 6;
-        int b =6;
-        if (a>=b) {
+        int b = 6;
+        if (a >= b) {
             System.out.println("a >= b");
-        }
-        else
+        } else
             System.out.println("a < b");
     }
 }
