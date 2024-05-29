f613ba0de9a8fa3bfe8e08fe4f8cc70b4998dd2d
Maksim Nadtochaev <maksim.nadtochaev@gmail.com>
2024-05-29 21:32:00 +0300

    Update task1.java

--- src/task1.java
+++ src/task1.java
@@ -1,5 +1,8 @@
 public class task1 {
     public static void main(String[] args) {
+        printThreeWords();
+    }
+        public static void printThreeWords() {
         System.out.println("Orange\nBanana\nApple");
     }
 }
